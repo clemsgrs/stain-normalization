50afeec83c469818a50440bfc507d6db7251fe84
clems grisi <clementnet@outlook.com>
2021-02-08 00:03:52 +0100

    idk why --output doesn't work with colab

--- scripts/checkpoint_download.sh
+++ scripts/checkpoint_download.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-gdown --id 1boPBQa6ySc2jepmHmJAegVepb6l3K8Ib --output checkpoints/
-gdown --id 1almkTK7RcgyAZC_BUdEYcvnBpem949xH --output checkpoints/
+gdown --id 1boPBQa6ySc2jepmHmJAegVepb6l3K8Ib
+gdown --id 1almkTK7RcgyAZC_BUdEYcvnBpem949xH
