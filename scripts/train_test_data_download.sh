dd27ecab37627dfca0d1ae12797075447c91a293
clems grisi <clementnet@outlook.com>
2021-02-09 20:12:36 +0100

    split images in train/test so I have consist datasets across experiments

---
+++ scripts/train_test_data_download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+gdown --id 1640FtfGv1gQ3gdsEo5chEWb23qmJlhXm
+gdown --id 1-9D2Yn_JDprzv_VTugFfXKsaah7ECEaN
+gdown --id 1-BHbhFQAMCuISqRviytFYvyXK1O7AdCM
+gdown --id 1-BoubBOfmie0ZBJtkIfZOyuz4mtSoSpi
