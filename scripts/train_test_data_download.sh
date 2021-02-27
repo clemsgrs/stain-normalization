9a988ce0a1406ca6080da0de6fe43ce6d2aa5b4d
clems grisi <clementnet@outlook.com>
2021-02-27 13:03:15 +0100

    improved train_test_data_download.sh

--- scripts/train_test_data_download.sh
+++ scripts/train_test_data_download.sh
@@ -3,3 +3,11 @@ gdown --id 1640FtfGv1gQ3gdsEo5chEWb23qmJlhXm
 gdown --id 1-9D2Yn_JDprzv_VTugFfXKsaah7ECEaN
 gdown --id 1-BHbhFQAMCuISqRviytFYvyXK1O7AdCM
 gdown --id 1-BoubBOfmie0ZBJtkIfZOyuz4mtSoSpi
+unzip -q trainA.zip
+rm trainA.zip
+unzip -q trainB.zip
+rm trainB.zip
+unzip -q testA.zip
+rm testA.zip
+unzip -q testB.zip
+rm testB.zip
--- scripts/train_test_unzip.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-unzip trainA.zip
-unzip trainB.zip
-unzip testA.zip
-unzip testB.zip
-rm trainA.zip
-rm trainB.zip
-rm testA.zip
-rm testB.zip
