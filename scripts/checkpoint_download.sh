d708ee4cda7c86992961f0009e0ae6b595863c70
clems grisi <clementnet@outlook.com>
2021-02-27 12:53:03 +0100

    improved checkpoint download

--- scripts/checkpoint_download.sh
+++ scripts/checkpoint_download.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 gdown --id 1daGZpTqBQTs9riCkO8Fly0lBnR3Ag_EI -O checkpoints/checkpoints.zip
-unzip checkpoints/checkpoints.zip
+unzip -q checkpoints/checkpoints.zip
+rm checkpoints/checkpoints.zip
